720689b94aebc42cb4b2a2e9dd6d7cecf7841d0a
Roman Zapatmar <89195729+romanzapatmar@users.noreply.github.com>
2024-09-04 12:20:50 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,4 @@
+import jf, {b, c, d} from './module2.mjs'
+console.log(jf);
+console.log(b);
+console.log(c);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Roman";
+const b = "Jay";
+const c = "Amit";
+const d = "Suraj";
+
+export default a;
+export {b};
+export {c};
+export {d};
